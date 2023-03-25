c8a85739fa24b526424b3824c653d99d2bda8931
Shadient <85789237+Shadient@users.noreply.github.com>
2023-03-25 15:19:37 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,27 @@
-sudo pacman -Sy python-pip
+# Python 3
+sudo pacman -Sy python-pip -y
+
+# Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Terminal Emulator
+sudo pacman -S alacritty -y
+
+# Tiling Window Manager
+sudo pacman -S bspwm sxhkd -y
+mkdir ~/.config/bspwm
+cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/
+mkdir ~/.config/sxhkd
+cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/
+
+# Compositor
+sudo pacman -S picom -y
+
+# Application Launcher
+sudo pacman -S rofi -y
+
+# Browser
+sudo pacman -S brave-browser -y
+
+# MISC & utils
+sudo pacman -S nitrogen -y
