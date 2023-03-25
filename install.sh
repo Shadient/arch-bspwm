0613769db20229e2ba2691ae70b7b915d02e6021
Shadient <85789237+Shadient@users.noreply.github.com>
2023-03-25 15:43:45 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,6 +14,8 @@ cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/
 mkdir ~/.config/sxhkd
 cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/
 
+cp /etc/X11/xinit/xinitrc .xinitrc
+
 # Compositor
 sudo pacman -S picom -y
 
