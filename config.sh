1ab0785591d90eeb48e4c25357fb27d27a1f6853
Shadient <85789237+Shadient@users.noreply.github.com>
2023-03-26 12:12:40 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,1 +1,10 @@
+#!/usr/bin/env bash
 
+# BSPWM
+mkdir ~/.config/bspwm
+cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/
+curl https://raw.githubusercontent.com/Shadient/arch-bspwm/main/bspwm/bspwmrc > ~/.config/bspwm/bspwmrc
+
+mkdir ~/.config/sxhkd
+cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/
+curl https://raw.githubusercontent.com/Shadient/arch-bspwm/main/sxhkd/sxhkdrc > ~/.config/sxhkd/sxhkdrc
